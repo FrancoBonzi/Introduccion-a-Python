7bc77c1ef8d85765371ddac57c3e56cf37b4d67e
FrancoBonzi <bon.franco12@gmail.com>
2025-10-27 12:28:03 -0300

    Modificaciones en practica2.py

--- practica2.py
+++ practica2.py
@@ -1,18 +1,18 @@
-print("=== La Trifecta ===")
+print("=== LA TRIFECTA ===")
 
 while True:
-    numero = input("\nIngrese un número entero (0 para salir): ")
+    numero = input("\nIngrese un número entero positivo (0 para salir): ")
 
     
     if not numero.isdigit():
-        print("Error: debe ingresar un número entero.")
+        print("Error: debe ingresar un número válido.")
         break
 
     numero = int(numero)
 
     
     if numero == 0:
-        print("Programa finalizado por el ingreso del 0.")
+        print("Programa finalizado, ingresó 0.")
         break
 
     
